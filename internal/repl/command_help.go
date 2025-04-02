d28191cc78d606e61debdef099ef6e26e8d097e1
Jeremy Hughes <jthughes@users.noreply.github.com>
2025-04-02 18:03:22 +1030

    Added goreleaser config and updating install instructions in README

--- internal/repl/command_help.go
+++ internal/repl/command_help.go
@@ -3,7 +3,7 @@ package repl
 import "fmt"
 
 func commandHelp(config *Config, args []string) error {
-	fmt.Println("Welcome to GoGroovy!")
+	fmt.Println("Welcome to disgo!")
 	fmt.Println("Usage:")
 	fmt.Println()
 	for _, command := range commands {
