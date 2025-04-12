f5fcbbe72c4574e44e5b4ee00c37ddfe8d347fc0
Jeremy Hughes <jthughes@users.noreply.github.com>
2025-04-12 14:30:26 +0930

    Added missing new line in error message

--- internal/repl/command_goto.go
+++ internal/repl/command_goto.go
@@ -12,7 +12,7 @@ func commandGoto(config *Config, args []string) error {
 	}
 	targetPosition, err := strconv.Atoi(args[0])
 	if err != nil || targetPosition < 0 || targetPosition > len(config.player.Playlist) {
-		fmt.Printf("Error: Needs to be in range [0, %d]", len(config.player.Playlist)-1)
+		fmt.Printf("Error: Needs to be in range [0, %d]\n", len(config.player.Playlist)-1)
 		return nil
 	}
 	offset := targetPosition - config.player.PlaylistPosition
